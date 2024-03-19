f8f09cf9811a8b488b4b45bc06b6aba0098e0b1b
Jonathan Frankel <beans@habanerojam.xyz>
2024-03-19 18:12:11 +0800

    adding colorscheme and save with leader w

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- save file
+vim.keymap.del("n", "<leader>ww")
+vim.keymap.del("n", "<leader>wd")
+vim.keymap.del("n", "<leader>w-")
+vim.keymap.del("n", "<leader>w|")
+
+vim.keymap.del({ "i", "x", "n", "s" }, "<C-s>")
+
+vim.keymap.set("n", "<leader>w", "<cmd>w<cr><esc>", { desc = "Save file" })
